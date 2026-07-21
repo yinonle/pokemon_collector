c9f0f51282c659adb0b2fd3c86e6538bf59f72e8
Yinon Levi <yinonle@users.noreply.github.com>
2026-07-21 14:06:28 +0300

    Add message_processor.py to handle message processing logic

--- sqs_in/consumer.py
+++ sqs_in/consumer.py
@@ -1,16 +1,13 @@
-import boto3
-from moto import mock_aws
-
 from scheme.validate_requests import get_sqs_client
 
-
+#Input url_sqs and I return the messages
 def receive_messages(queue_url):
     client = get_sqs_client()
 
     response = client.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=10) 
     message = response.get("Messages", [])
     return message
-    
-    
-        
+
+#raw_message = receive_messages(queue_url)
+#       
     
