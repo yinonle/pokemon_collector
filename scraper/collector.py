b46472d20b210a815ece4666399648fbff10023f
Yinon Levi <yinonle@users.noreply.github.com>
2026-07-29 11:10:12 +0300

    Finish to write extract_identifiers

--- scraper/collector.py
+++ scraper/collector.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple, Union
 
 from DB.pokedex import PokedexRepository
 from scraper.scraper import PokemonScraper
@@ -11,30 +11,45 @@ class CollectorService:
     def __init__(self, 
                  pokedex_repo: PokedexRepository = None,
                   scraper: PokemonScraper = None, 
-                  backup_handler: JsonBackupFile = None):
+                  backup_file: JsonBackupFile = None):
         
         self.pokedex_repo = pokedex_repo or PokedexRepository()
         self.scraper = scraper or PokemonScraper()
-        self.backup_handler = backup_handler or JsonBackupFile()
+        self.backup_file = backup_file or JsonBackupFile()
 
 
-    def _extract_identifiers(self, request: SqsMessage):
-        identifiers = []
+    
+     
+
+    def _extract_identifiers(self, request: SqsMessage) -> Tuple[List[Union[int, str]], List[str]]:
+        valid_identifiers = []
         failed_list = []
 
         if isinstance(request, NameCollectionRequest):
-            
+            valid_identifiers.append(request.p_name)
 
         elif isinstance(request, NumberCollectionRequest):
-            pass
+            num = request.p_number
+            if 1 <= num <= 100:
+                valid_identifiers.append(num)
+            else:
+                failed_list.append(str(num))
 
         elif isinstance(request, RangeCollectionRequest):
-            pass
+            start_str, end_str = request.p_range.split("-")
+            start, end = int(start_str), int(end_str)
+            for num in range(start, end + 1):
+                if 1 <= num <= 100:
+                    valid_identifiers.append(num)
+                else:
+                    failed_list.append(str(num))
 
-        return identifiers, failed_list
+        return valid_identifiers, failed_list
+
+
 
     def process_collection_request(self, request_data: Dict[str, Any]) -> CollectorOutputResponse:
-        identify()
+        
         
 
 
